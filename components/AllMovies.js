cafd69b0f57f338b6240712950710641eace2901
Kai Xin <22020771@myrp.edu.sg>
2024-11-11 15:23:07 +0800

    Lesson 2 movies

---
+++ components/AllMovies.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {ScrollView} from 'react-native';
+import Movie from './Movie';
+
+const AllMovies = () => {
+    return (
+        <ScrollView>
+            <Movie title="Doctor sleep" year={2019} icon_name="skull" poster={require("../img/doctor-sleep.jpg")}/>
+            <Movie title="Midway" year={2020} icon_name="person-rifle" poster={require("../img/midway.jpg")}/>
+        </ScrollView>
+    );
+};
+
+export default AllMovies;
